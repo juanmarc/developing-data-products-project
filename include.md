e02c178f102f212e1c4dc7685bab29091c862598
Marc H <marc.hidalgo@cox.net>
2016-08-28 12:45:28 -0500

    Modifications for tab panels and addition of readme file

---
+++ include.md
@@ -0,0 +1,15 @@
+This application allows users to view statistics for Major League Baseball (MLB) teams over the period 1969 - 2000. Users may select 1 or more MLB teams along with one team statistic to explore.
+
+
+- Data are viewable by MLB franchises that were in existence during that period (for reference, see [MLB Franchise History](https://en.wikipedia.org/wiki/Timeline_of_Major_League_Baseball)).
+
+- Resultant plots are color coded by general team colors to allow easier inspection of the data. 
+
+- While users may select up to all 30 MLB teams, the plots will become harder to read as more teams are selected. It is recommended that users limit their team selections to 4 or less.
+
+- Data for the entire period is plotted for the selected teams(s), but, in some cases (e.g. Blue Jays, Rockies, Marlins), the team came into existence sometime during 1969 - 2000. In these cases, data will only be plotted for the year the franchise existed.
+
+
+Data used for the application are associated with the UsingR library of the R programming language [UsingR](https://cran.r-project.org/web/packages/UsingR/) available from [Comprehensive R Archive Network](https://cran.r-project.org/). 
+
+Source code is available on  [GitHub](https://github.com/juanmarc/developing-data-products-project).
--- ui.R
+++ ui.R
@@ -4,36 +4,41 @@ library(data.table)
 library(UsingR)
 
 shinyUI(
-  #specify a layout for page with a sidebar
-  pageWithSidebar(
-    
-    #instantiate a header panel with an overall title
-    headerPanel( p("MLB Team Stats Explorer") ),
-  
-    #instanditate the sidebard to control team data to be plotted
-    sidebarPanel(width=3, uiOutput("teamControls") ),
- 
-    #in the main panel, allow the user to select the relevant statistics and plot the outcome
-    mainPanel(
-      #the first column will include a selector for data to be plotted
-      column(3,
-        wellPanel(
-          radioButtons(
-            "statsToBeExplored", "Stat To Explore (chose 1):",
-            c("Attendance" = "attendance", "Runs Scored" = "scored", 
-               "Runs Allowed" = "allowed", "Wins" = "wins") 
-          ) #end of radioButtons
-        ) #end of wellpanel      
-      ), #end of column
-      #the second column will inlcude the plots along with a legend for the team colors
-      column(9,
-        wellPanel( 
-          plotOutput('teamStatByYear'),
-          plotOutput('teamLegend')
-        ) #end of wellPanel 
-      ) #end of column
-    ) #end of mainPanel
+  #specify a layout for the page with tabs
 
-  ) #end of pageWithSidebar
+  navbarPage(p("MLB Team Stats Explorer"),
+
+    #instantiate a tab for the plotted data
+    tabPanel("Plot",         
+             
+      #instanditate the sidebar to control team data to be plotted
+      sidebarPanel(width=3, uiOutput("teamControls") ),
+ 
+      #in the main panel, allow the user to select the relevant statistics and plot the outcome
+      mainPanel(
+        #the first column will include a selector for data to be plotted
+        column(3,
+          wellPanel(
+            radioButtons(
+              "statsToBeExplored", "Stat To Explore (chose 1):",
+              c("Attendance" = "attendance", "Runs Scored" = "scored", 
+                "Runs Allowed" = "allowed", "Wins" = "wins") 
+            ) #end of radioButtons
+          ) #end of wellpanel      
+        ), #end of column
+        #the second column will inlcude the plots along with a legend for the team colors
+        column(9,
+          wellPanel( 
+            plotOutput('teamStatByYear'),
+            plotOutput('teamLegend')
+          ) #end of wellPanel 
+        ) #end of column
+      ) #end of mainPanel
+    ), #end of plot tab panel
+  
+    #instantiate an "About" tab help describe the app
+    tabPanel("About", mainPanel(includeMarkdown("include.md") ) )
+  
+  ) #end of navbarPage
   
 ) #end of shinyUI
